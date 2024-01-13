df64e91147160d9b5eed6a8f7e6fd3a5f0af678f
Gerson Garayar <garayargerson20@gmail.com>
2024-01-13 11:51:41 -0500

    fix: arreglando logica

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,36 @@
 import { useState, useRef } from "react";
 
 export default function App() {
-  const resizableElementRef = useRef<HTMLElement | null>(null);
+  const containerRef = useRef<HTMLElement>(null);
   const [w, setW] = useState(200);
   const [move, setMove] = useState(200);
 
-  const handleMouseDown = (event: any) => {
+  const handleMouseDown = (event: any, direction: string) => {
     const startX = event.clientX;
 
     const handleMouseMove = (event: any) => {
-      if (resizableElementRef.current !== null) {
-        const deltaX = event.clientX - startX;
-        console.log(deltaX * 2);
-        setMove(deltaX * 2);
-        setW(w + move);
+      if (containerRef.current !== null) {
+        if (direction === "right") {
+          const deltaX = event.clientX - startX;
+          setMove(deltaX * 2);
+          setW(
+            w + move < 200
+              ? 200
+              : w + move > containerRef.current.offsetWidth
+              ? containerRef.current.offsetWidth
+              : w + move
+          );
+        } else {
+          const deltaX = event.clientX - startX;
+          setMove(-deltaX * 2);
+          setW(
+            w + move < 200
+              ? 200
+              : w + move > containerRef.current.offsetWidth
+              ? containerRef.current.offsetWidth
+              : w + move
+          );
+        }
       }
     };
 
@@ -28,19 +45,18 @@ export default function App() {
 
   return (
     <main className="bg-black h-screen">
-      <section className="border h-full">
+      <section ref={containerRef} className="border h-full">
         <article
           className="h-96 m-auto relative select-none"
-          ref={resizableElementRef}
-          style={{ maxWidth: w + move }}
+          style={{ maxWidth: w + move, minWidth: 200 }}
         >
           <div
             className="absolute left-0 top-1/2 w-4 h-20 -translate-y-10 border bg-green-300 hover:bg-green-400 active:bg-green-500 cursor-e-resize"
-            onMouseDown={handleMouseDown}
+            onMouseDown={(e) => handleMouseDown(e, "left")}
           ></div>
           <div
             className="absolute right-0 top-1/2 w-4 h-20 -translate-y-10 border bg-red-300 hover:bg-red-400 active:bg-red-500 cursor-e-resize"
-            onMouseDown={handleMouseDown}
+            onMouseDown={(e) => handleMouseDown(e, "right")}
           ></div>
           <div className="border m-4 h-full shadow-2xl text-white">
             contenido
